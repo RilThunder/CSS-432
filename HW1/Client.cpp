36c39e91d1f3cb8e6e67dc94693c10b77389e846
Unknown <oda1234@uw.edu>
2018-04-01 23:51:11 -0700

    Finished the source code
    Implemented Client and Server source code. Working on creating scripts to automate writing data to a text file for analysis

---
+++ HW1/Client.cpp
@@ -0,0 +1,207 @@
+//
+// Created by thuan on 30/03/18.
+//
+
+#include <iostream>
+#include <netdb.h>
+#include <cstring>
+#include <sys/time.h>
+#include <unistd.h>
+
+using namespace std;
+
+const int TOTAL_SIZE = 1500;
+const int MULTIPLE_WRITE = 1;
+const int WRITEV = 2;
+const int SINGLE_WRITE = 3;
+
+int port;
+int repetition;
+int numberBuffer;
+int bufferSize;
+char *serverIPName;
+int type;
+
+/**
+ * This function is used to check for valid input from the command line arguments
+ * Valid port should be between 1024 and 65535
+ * Repetition should be at least 0
+ * Number Buffer * buffer size should be 1500
+ * Only allow 3 type of input
+ * @param argumentValues The command line argument parsed from main
+ * @return -1 for invalid input and 0 for valid input
+ */
+int checkValidInput(char *argumentValues[])
+{
+    // stoi is better than atoi since it will throw exception if it cannot convert
+    try
+    {
+        port = stoi(argumentValues[ 1 ]);
+        repetition = stoi(argumentValues[ 2 ]);
+        numberBuffer = stoi(argumentValues[ 3 ]);
+        bufferSize = stoi(argumentValues[ 4 ]);
+        type = stoi(argumentValues[ 6 ]);
+    }
+    catch (invalid_argument &exception) {
+        cout << "Incorrect data was entered. Should enter valid integer only";
+        cout << exception.what();
+        return -1;
+    }
+    catch (out_of_range &exception) {
+        cout << "Incorrect range of data was entered. Should only entere within the range of integer";
+        cout << exception.what();
+        return -1;
+    }
+    serverIPName = argumentValues[ 5 ];
+    if ( port < 1024 || port > 65535 )
+    {
+        cout << "Invalid port number defined. Need to be between 1024 and 65535";
+        return -1;
+    }
+    if ( repetition < 0 )
+    {
+        cout << " Invalid number of repetition entered. Should be at least 0 (no repetition)";
+        return -1;
+    }
+    if ( numberBuffer * bufferSize != TOTAL_SIZE )
+    {
+        cout << "Invalid number of data buffer or data buffer size. When multiply, should be equal " +
+                to_string(TOTAL_SIZE);
+        return -1;
+    }
+    if ( string(serverIPName).empty())
+    {
+        cout << "Empty string detected as server name";
+        return -1;
+    }
+    if ( type < 1 || type > 3 )
+    {
+        cout << "Invalid transfer scenario. Only accept " + to_string(MULTIPLE_WRITE) + ", " + to_string(WRITEV)
+                + " or " + to_string(SINGLE_WRITE);
+        return -1;
+    }
+    return 0;
+}
+
+/**
+ * This function is used to print out the result for sending data from the client to server and from server back to client
+ * It will print out the time it took to send to server (from start until it write to server)
+ * It will print out the time it took to receive response back from server (from start until it read back data from server)
+ * @param start Starting time that it begin to prepare the data depending on different type
+ * @param lap Time that it send the data to the server
+ * @param end Time that it receive data back from the server
+ * @param numberOfRead The number of read the server receives
+ */
+void printResult(struct timeval start , struct timeval lap , struct timeval end , int numberOfRead)
+{
+    // Second to microsecond needs to multiply by 1000000
+    long dataSendingTime = (lap.tv_sec - start.tv_sec) * 1000000 + lap.tv_usec - start.tv_usec;
+    long roundTripTime = (end.tv_sec - start.tv_sec) * 1000000 + lap.tv_usec - start.tv_usec;
+    cout << "Test 1: ";
+    cout << "data-sending time = " + to_string(dataSendingTime) + " usec, ";
+    cout << "round-trip time = " + to_string(roundTripTime) + " usec, ";
+    cout << "Number of reads =  " + to_string(numberOfRead);
+    return;
+}
+
+/**
+ * Main method of the client. Take in input from the command line and make a connection to the server
+ * @param argumentNumber The number of arguments that it received
+ * @param argumentValues a char array that contains the data at each index
+ * @return -1 for failure and 0 for success
+ */
+int main(int argumentNumber , char *argumentValues[])
+{
+    // Expected to receive 6 arguments plus the invocation name of the program which make it 7
+    if ( argumentNumber != 7 )
+    {
+        cout << "Incorrect number of arguments provided";
+        return -1;
+    }
+    int checkResult = checkValidInput(argumentValues);
+    if ( checkResult == -1 )
+    {
+        cout << "Invalid argument supplied. Please enter argument values";
+        return -1;
+    }
+    char databuf[numberBuffer][bufferSize];
+    struct addrinfo hints; //define what the getaddrinfo going to do. Define IPV4 or v6, what kind of connection,..etc
+    struct addrinfo *serverInfo; // getaddrinfo will put the results in here. And we can go though this to get the address
+    memset(&hints , 0 , sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // either IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+    int addrInfoStatus = getaddrinfo(serverIPName , argumentValues[ 1 ] , &hints , &serverInfo);
+    if ( addrInfoStatus != 0 )
+    {
+        cout << "Unable to connect";
+        cout << gai_strerror(addrInfoStatus); // print out error message
+        return -1;
+    }
+    struct addrinfo *possibleConnection;
+    int socketFileDescriptor;
+    int socketConnectionResult;
+    // Go through all connection that was found and connect to the first one
+    for ( possibleConnection = serverInfo;
+          possibleConnection != NULL; possibleConnection = possibleConnection->ai_next )
+    {
+        socketFileDescriptor = socket(possibleConnection->ai_family , possibleConnection->ai_socktype ,
+                                      possibleConnection->ai_protocol);
+        if ( socketFileDescriptor == -1 )
+        {
+            cout << "Invalid one socket file descriptor detected. Looking for next one";
+            continue;
+        }
+        socketConnectionResult = connect(socketFileDescriptor , possibleConnection->ai_addr ,
+                                         possibleConnection->ai_addrlen);
+        if ( socketConnectionResult == -1 )
+        {
+            cout << "Invalid one socket connection result detected. Looking for next one";
+            continue;
+        }
+        cout << "Found a connection. Breaking out";
+        break;
+    }
+    // If still null, then it means that we went through all possible connections but none satisfied
+    if ( possibleConnection == NULL )
+    {
+        cout << "Unable to connect or empty result was given";
+        return -1;
+    }
+    freeaddrinfo(serverInfo);
+    struct timeval start , lap , end;
+    // having one repetition mean it will do at least 1 time. repetition = 1 mean it will do first time, then repeat one time
+    // having 0 repetition. Means it will not only do one time. But no repeat
+    gettimeofday(&start , NULL);
+    for ( int i = 0; i <= repetition; i++ )
+    {
+        if (type == 1) {
+            for ( int j = 0; j < numberBuffer; j++ )
+            {
+                write(socketFileDescriptor , databuf[ j ] , bufferSize);    // sd: socket descriptor
+            }
+        }
+        if (type == WRITEV) {
+            struct iovec vector[numberBuffer];
+            for ( int j = 0; j < numberBuffer; j++ )
+            {
+                vector[ j ].iov_base = databuf[ j ];
+                vector[ j ].iov_len = bufferSize;
+            }
+            writev(socketFileDescriptor , vector , numberBuffer);           // sd: socket descriptor
+        }
+        if (type == SINGLE_WRITE)
+        {
+            write(socketFileDescriptor , databuf , numberBuffer * bufferSize); // sd: socket descriptor
+        }
+    }
+    gettimeofday(&lap, nullptr);
+    int numberOfRead;
+    read(socketFileDescriptor , &numberOfRead , sizeof(numberOfRead));
+    gettimeofday(&end , nullptr);
+    printResult(start , lap , end , numberOfRead);
+    return 0;
+}
+
+
+
+
---
+++ HW1/Server.cpp
@@ -0,0 +1,193 @@
+//
+// Created by thuan on 30/03/18.
+//
+using namespace std;
+
+#include <iostream>
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netdb.h>
+#include <cstring>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+const int CONNECTION_REQUEST_SIZE = 10;
+const int BUFFSIZE = 1500;
+const string THREAD_MESSAGE = "Creating new thread with count: ";
+
+int port;
+int repetition;
+
+// Used to pass data to the thread
+struct thread_data
+{
+    int thread_id;
+    int clientFileDescriptor;
+};
+
+/**
+ * Used to check for valid input from the command line which take in a port number and number of repetition
+ * @param argumentValues the values from the command line
+ * @return -1 for failure and 0 for success
+ */
+int checkValidInput(char *argumentValues[])
+{
+    // stoi is better than atoi since it will throw exception if it cannot convert
+    try
+    {
+        port = stoi(argumentValues[ 1 ]);
+        repetition = stoi(argumentValues[ 2 ]);
+    }
+    catch ( invalid_argument &exception )
+    {
+        cout << "Incorrect data was entered. Should enter valid integer only";
+        cout << exception.what();
+        return -1;
+    }
+    catch ( out_of_range &exception )
+    {
+        cout << "Incorrect range of data was entered. Should only entere within the range of integer";
+        cout << exception.what();
+        return -1;
+    }
+    if ( port < 1024 || port > 65535 )
+    {
+        cout << "Invalid port number defined. Need to be between 1024 and 65535";
+        return -1;
+    }
+    if ( repetition < 0 )
+    {
+        cout << " Invalid number of repetition entered. Should be at least 0 (no repetition)";
+        return -1;
+    }
+    return 0;
+
+}
+
+/**
+ * This function is used to read data from a client and calculate the elapsed time it takes to finish that
+ * @param threadData the data that the thread is gonna use (client file descriptor and id)
+ * @return Nothing
+ */
+void *benchMark(void *threadData)
+{
+    char databuf[BUFFSIZE];
+    struct thread_data *data;
+    data = (struct thread_data *) threadData;
+    struct timeval start;
+    struct timeval stop;
+    int count = 0;
+    gettimeofday(&start , nullptr);
+    //
+    for ( int i = 0; i <= repetition; i++ )
+    {
+        for ( int numberRead = 0;
+              (numberRead += read(data->clientFileDescriptor , databuf , BUFFSIZE - numberRead)) < BUFFSIZE;
+              ++count );
+    }
+    gettimeofday(&stop , nullptr);
+    long totalTime = (stop.tv_sec - start.tv_sec) * 1000000 + (stop.tv_usec - start.tv_usec);
+    write(data->clientFileDescriptor , &count , sizeof(count));
+    cout << "Data-receiving time for thread " + to_string(data->thread_id) + " = " + to_string(totalTime) + " usec"
+         << endl;
+    cout << "Finish with thread " + to_string(data->thread_id) << endl;
+    // finish with this client. close it
+    close(data->clientFileDescriptor);
+}
+
+
+/**
+ * Main entry point of the program. Take in a port number and the number of repetition
+ * @param argumentNumber number of arguments passed in the command line
+ * @param argumentValues char array represent the argument at each index
+ * @return -1 for failure. Else keep running
+ */
+int main(int argumentNumber , char *argumentValues[])
+{
+    // C++ count the name of the program a.out as the first value
+    if ( argumentNumber != 3 )
+    {
+        cout << "Incorrect number of argument entered. Need to enter a port number and also the number of repetition";
+        return -1;
+    }
+    int checkResult = checkValidInput(argumentValues);
+    if ( checkResult == -1 )
+    {
+        cout << "Invalid argument supplied. Please enter argument values";
+        return -1;
+    }
+    struct addrinfo hints; // define how the server will be configure
+    struct addrinfo *serverInfo; // used to store all the connections that the server can use
+    memset(&hints , 0 , sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // IPv4 or v6
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    int addressInfoStatus = getaddrinfo(nullptr , argumentValues[ 1 ] , &hints , &serverInfo);
+    if ( addressInfoStatus != 0 )
+    {
+        cout << "Unable to connect";
+        cout << gai_strerror(addressInfoStatus); // print out error message
+        return -1;
+    }
+    int socketFileDescriptor;
+    int serverBindResult;
+    struct addrinfo *possibleConnection;
+    // go through all possible connection server can use. Use the first one that is appropriate
+    for ( possibleConnection = serverInfo;
+          possibleConnection != nullptr; possibleConnection = possibleConnection->ai_next )
+    {
+        socketFileDescriptor = socket(possibleConnection->ai_family , possibleConnection->ai_socktype ,
+                                      possibleConnection->ai_protocol);
+        if ( socketFileDescriptor == -1 )
+        {
+            cout << "Invalid one socket file descriptor detected. Looking for next one";
+            continue;
+        }
+        serverBindResult = bind(socketFileDescriptor , possibleConnection->ai_addr , possibleConnection->ai_addrlen);
+        if ( serverBindResult == -1 )
+        {
+            cout << "Unable to bind to the socket using this file descriptior";
+            close(socketFileDescriptor);
+            continue;
+        }
+        break; // at this point, have successfully bind to a socket
+    }
+    if ( possibleConnection == NULL )
+    {
+        cout << "Unable to connect or empty result was given";
+        return -1;
+    }
+    freeaddrinfo(serverInfo);
+    int listenUsingSocketResult = listen(socketFileDescriptor , CONNECTION_REQUEST_SIZE);
+    if ( listenUsingSocketResult != 0 )
+    {
+        cout << "Unable to listen using the socket file descriptor";
+        return -1;
+    }
+    int count = 1;
+    // Keep looping and listening to possible connection
+    while ( true )
+    {
+        struct sockaddr_storage clientSocket;
+        socklen_t clientSocketSize = sizeof(clientSocket);
+        int clientFileDescriptor = accept(socketFileDescriptor , (struct sockaddr *) &clientSocket , &clientSocketSize);
+        if ( clientFileDescriptor == -1 )
+        {
+            cout << "Unable to connect to client. Trying again";
+            continue;
+        }
+        pthread_t new_thread;
+        struct thread_data data;
+        data.thread_id = count;
+        data.clientFileDescriptor = clientFileDescriptor;
+        cout << THREAD_MESSAGE + to_string(count);
+        int threadResult = pthread_create(&new_thread , nullptr , benchMark , (void *) &data);
+        if ( threadResult != 0 )
+        {
+            cout << "Unable to create thread. Trying again";
+            continue;
+        }
+        count++;
+    }
+}
+
